38db9e58912f5bd230048fd052cabb8e325a4fef
giabach99 <giabach060399@gmail.com>
2023-05-18 15:28:03 +0700

    added service worker and media queries

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useEffect, useRef, useLayoutEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
 import {Routes, Route, useNavigate} from "react-router-dom";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
 import { checkUserSession } from "./store/user/user.action";
-
+import Spinner from "./components/spinner/spinner.component";
 
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "./store/user/user.selector";
+import { GlobalStyle } from "./global.styles";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+
 
 const App = () => {
   const dispatch = useDispatch();
@@ -36,22 +39,19 @@ const App = () => {
         // return unsubscribe;
   }, [])
 
-  
-
-  
-  
-
-
   return (
-    <Routes>
-      <Route path='/' element={<Navigation/>}>
-        <Route index element={<Home/>}/>
-        <Route path="shop/*" element={<Shop/>}/>
-        {/* <Route path='auth' render={() => ( !user ? <Authentication /> : <Navigate to='/' /> )}/> */}
-        <Route path='auth' element={<Authentication/>}/>
-        <Route path='checkout' element={<Checkout/>}/>
-      </Route>
-    </Routes>    
+    <Suspense fallback={<Spinner/>}>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Navigation/>}>
+          <Route index element={<Home/>}/>
+          <Route path="shop/*" element={<Shop/>}/>
+          {/* <Route path='auth' render={() => ( !user ? <Authentication /> : <Navigate to='/' /> )}/> */}
+          <Route path='auth' element={<Authentication/>}/>
+          <Route path='checkout' element={<Checkout/>}/>
+        </Route>
+      </Routes>
+    </Suspense> 
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,6 +10,8 @@ import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import {Elements} from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -27,6 +28,8 @@ root.render(
     </Provider>     
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
